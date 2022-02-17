87f95c699d7e92de97a6a12abc8bf46dd9cb3857
renato-romankevicius <99765208+renato-romankevicius@users.noreply.github.com>
2022-02-17 14:42:57 -0300

    Add files via upload

--- Exercise on GitHub.py
+++ Exercise on GitHub.py
@@ -1,6 +1,6 @@
 # Python Program to Make a Simple Calculator
 
-def multipication(num1, num2):
+def multiplication(num1, num2):
     return num1 * num2
 
 
@@ -8,30 +8,55 @@ def addition(num1, num2):
     return num1 + num2
 
 
-def subtraction(num2, num1):
+def subtraction(num1, num2):
     return num1 - num2
 
 
-def divide(num1, num1):
+def divide(num1, num2):
     return num1 / num2
 
-
+# Creating a function for integer number input
+def intin(prmpt):
+    while True:
+        try:
+            num = int(input(prmpt))
+            break
+        except ValueError:
+            print('Please enter a integer number')
+    return num
+# Options selection and calculation
+operation = 0
 while True:
-    value1 = int(input("Enter 1st number: "))
-    value2 = int(input("Enter 2nd number: '))
+    while True:
+        print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction, 5-Quit")
+        try:
+            operation = int(input("Choose operation 1/2/3/4/5: "))
+            if 0 < operation < 6:
+                break
+            else:
+                print('Please enter a valid option')
+        except ValueError or RuntimeError:
+            print('Please enter a valid option')
 
-    print("Select operation 1-Division, 2-Multiplication, 3-Addition, 4-Subtraction")
-
-    operation = int(input("Choose operation 1/2/3/4: ")
-                    if operation == 1
-    print(value1, "/", value2, "=", divide(value1, value2))
-
-    elif operation == 2:
-    print(value1, "*", value2, "=", multiplication(value1, value2))
-    elif operation = 3:
-    print(value1, "+", value2, "=", addition(value1, value2))
-    elif operation == 4:
-    print(value1, "-", value2, "=", subtraction(value1, value2)))
-
+    if operation == 5:
+        break
     else:
-    print("Enter correct operation")
+        value1 = intin('Please enter first number: ')
+        flag = 0
+        while flag == 0:
+            value2 = intin('Please enter second number: ')
+            if operation == 1 and value2 == 0:
+                print('Attention! the denominator shall not be zero')
+            else:
+                flag = 1
+#  Execute the chosen operation
+    if operation == 1:
+        print(value1, "/", value2, "=", divide(value1, value2))
+    elif operation == 2:
+        print(value1, "*", value2, "=", multiplication(value1, value2))
+    elif operation == 3:
+        print(value1, "+", value2, "=", addition(value1, value2))
+    elif operation == 4:
+        print(value1, "-", value2, "=", subtraction(value1, value2))
+    else:
+        print("Enter correct operation")
